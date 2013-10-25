435d32979dde08a0aa46d61c222012c92082191c
Antonio C Nalesso Moreira <acnalesso@yahoo.co.uk>
2013-10-25 14:51:57 +0100

    AnagramSolver::Middleware is now ready to precompute files and search for anagrams

--- lib/anagram_solver/middleware.rb
+++ lib/anagram_solver/middleware.rb
@@ -1,3 +1,9 @@
+require 'rack/request'
+
+##
+# TODO:
+# Break this down, as it is doing
+# alot.
 module AnagramSolver
   class Middleware
     attr_reader :app
@@ -6,8 +12,95 @@ module AnagramSolver
       @app = app
     end
 
+    ##
+    # Assigns env to @env to be used later on.
+    # If PATH_INFO matches search, it then tries
+    # to find anagrams. In not it goes down the stack.
+    #
     def call(env)
-      app.call(env)
+      @env = env
+      uploaded_tempfile?
+      search? || app.call(env)
     end
+
+    ##
+    # Returns a response which is a JSON response
+    # if search_path?
+    #
+    # When trying to find an anagram AnagramSolver::Finder
+    # either returns the anagrams found for a particular word or
+    # it returns a notice (i.e a message ) saying that No anagrams
+    # was found for that particular word.
+    #
+    def search?
+      return response if search_path?
+    end
+
+    private
+
+      attr_reader :env
+
+      ##
+      # True is PATH_INFO matches search,
+      # false otherwise.
+      #
+      def search_path?
+        env["PATH_INFO"] =~ /search/
+      end
+
+      ##
+      # Sets a 200 HTTP response code,
+      # Sets Content-Type to JSON, and
+      # returns anagrams_result.
+      #
+      def response
+        [200, { "Content-Type" => "application/json" }, [anagrams_result].to_json]
+      end
+
+
+      ##
+      # Returns results of anagrams.
+      # If none is found it returns a notice
+      # saying that no anagram was found.
+      #
+      def anagrams_result
+        Finder.find_for(word, @permutator)
+      end
+
+
+      ##
+      # Gets the json request sent to server
+      # then parse it and get the raw word.
+      #
+      def word
+        _word_ = JSON.parse(env["rack.input"].string)
+        _word_["word"]
+      end
+
+      ##
+      # Rack::Request rocks!!
+      #
+      def params
+        Rack::Request.new(env)
+      end
+
+      ##
+      # Gets the tempfile uploaded.
+      # Rack::Request parses and extract params
+      # from StringIO for us.
+      #
+      def tempfile
+        params["new_anagram"]["dict"][:tempfile]
+      end
+
+      def uploaded_tempfile?
+        permutator if (env["PATH_INFO"] == "/anagrams")
+      end
+
+
+      def permutator
+        @permutator = Permutator.new(tempfile)
+      end
+
   end
 end
--- spec/lib/anagram_solver/middleware_spec.rb
+++ spec/lib/anagram_solver/middleware_spec.rb
@@ -1,9 +1,39 @@
 require 'spec_helper'
+require 'json'
+require 'stringio'
+require 'rack/mock'
 require 'anagram_solver/middleware'
 
+module AnagramSolver
+  Middleware.class_eval do
+    public( :word, :params, :tempfile,
+            :uploaded_tempfile?, :permutator )
+  end
+
+  class Finder
+    def self.find_for(word, obj); end
+  end
+
+  class Permutator
+    def initialize(words)
+      words
+    end
+  end
+end
+
 describe AnagramSolver::Middleware do
+  let(:env) do
+    {
+      "PATH_INFO" => "/anagrams_search",
+      "rack.input" => StringIO.new('{ "word": "test"}'),
+      "new_anagram" => { "dict" => { tempfile: "text.txt" } }
+    }
+  end
+
   let(:ted) { double("Ted") }
   let(:waremiddle) { AnagramSolver::Middleware.new(ted) }
+  let(:app) { ->(*args) { args } }
+  let(:rack) { AnagramSolver::Middleware.new(app) }
 
   context "Middleware" do
     it "must respond_to :call" do
@@ -25,5 +55,102 @@ describe AnagramSolver::Middleware do
       response = waremiddle.call(:next)
       response.should be_an Array
     end
+
+    context "Uploading a file","Permutator" do
+
+      before {
+        env["PATH_INFO"] = "/anagrams"
+        rack.stub(:env).and_return(env)
+        rack.stub(:params).and_return(env)
+      }
+
+      it { rack.should respond_to :permutator }
+
+      it { rack.permutator.should be_kind_of AnagramSolver::Permutator }
+
+      it {
+        rack.call(env)
+        rack.instance_variable_get(:@permutator).should_not be_nil
+      }
+
+      it { rack.should respond_to :params }
+      it { rack.should respond_to :tempfile }
+      
+      it {
+        rack.tempfile.should eq("text.txt")
+      }
+
+      it { rack.should respond_to :uploaded_tempfile? }
+
+      it "must parse PATH_INFO to see if a file is being uploaded" do
+        rack.uploaded_tempfile?.should be_true
+      end
+
+      it "must send tempfile to AnagramSolver::Permutator" do
+        AnagramSolver::Permutator.stub(:new).with("text.txt")
+
+        rack.call(env)
+        AnagramSolver::Permutator.should have_received(:new)
+      end
+
+      it "must go down the stack" do
+        arnold = double
+        arnold.should_receive(:call).with(env)
+        rack.stub(:app).and_return(arnold) 
+
+        rack.call(env)
+      end
+    end
+
+    context "Anagrams" do
+      let(:tob) { AnagramSolver::Middleware.new env }
+
+      it "must get word to find anagrams for" do
+        tob.stub(:env).and_return(env)
+        tob.word.should eq("test")
+      end
+    end
+
+    describe "/search" do
+
+      let(:tim) { AnagramSolver::Middleware.new env }
+
+      before do
+        tim.stub(:env).and_return(env)
+        tim.should_not_receive(:call).with(env)
+      end
+
+      it "must search if search_path?" do
+        r = [200, { "Content-Type" => "application/json" }, ["opts"].to_json]
+
+        tim.stub(:anagrams_result).and_return("opts")
+        tim.search?.should eq(r)
+      end
+
+      context "Anagram found" do
+
+        let(:vendetta) { double }
+        let(:mid) { AnagramSolver::Middleware.new(vendetta) }
+
+        it "must not call the next middleware" do
+          mid.stub(:params).and_return(env)
+          vendetta.should_not_receive(:call)
+          mid.call(env)
+        end
+
+      end
+
+      context "Anagram not found" do
+
+        it "must return a notice" do
+          notice = "No anagrams found for :word"
+          AnagramSolver::Finder.stub(:find_for).and_return(notice)
+          r = [200, { "Content-Type" => "application/json" }, [notice].to_json]
+
+          tim.search?.should eq(r)
+        end
+      end
+    end
+
   end
 end
